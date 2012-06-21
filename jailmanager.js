a1a40a62441a610e635dfc12af37d411c73593b8
maneulyori <maneulyori@gmail.com>
2012-06-21 19:07:56 +0900

    Use wrench.js to copy directory

--- jailmanager.js
+++ jailmanager.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
+var wrench = require('wrench');
 var jails = [];
 var mountList = [];
 var jailDir = '/tmp/';
@@ -23,16 +25,18 @@ function createJail(sourceJail)
   //TODO: write regexp to filter all special character in sourceJail variable.
   //TODO: add random string generator.
   var i;
-  var destJail = Math.random() + '/'; //TODO: Write jail name generator;
+  var destJail = Math.random(); //TODO: Write jail name generator;
+  jails.push(destJail);
+  var ret = destJail;
+  destJail += '/';
   
-  var cp = spawn('cp', ['-r', sourceJail, jailDir + destJail]);
+  wrench.copyDirSyncRecursive(sourceJail, jailDir + destJail);
   
   for(i=0; i<mountList.length; i++)
   {
-    fs.mkdirSync(jailDir + destJail + mountList[i][1])
+    fs.mkdirSync(jailDir + destJail + mountList[i][1]);
     var mount = spawn('mount', ['-o', 'bind', mountList[i][0], jailDir + destJail + mountList[i][1]]);
   }
-  jails.push(destJail);
   
   return destJail;
 }
@@ -40,13 +44,24 @@ function createJail(sourceJail)
 function destroyJail(jail)
 {
   //TODO: add jail deleter.
+  var loop = true;
   var i;
   for(i=0; i<mountList.length; i++)
   {
-    var umount = spawn('umount', [jailDir + jail + mountList[i][1]]);
+    var umount = spawn('umount', [jailDir + jail + '/' + mountList[i][1]]);
   }
   
-  var rm = spawn('rm', ['-rf', jailDir + jail]);
+  while(loop)
+  {
+    loop = false;
+    try {
+      wrench.rmdirSyncRecursive(jailDir + jail);
+      console.log("Deleting...");
+    } catch (error) {
+      console.log("error detected.");
+      loop = true;
+    }
+  }
 }
 
 function destroyAll()
